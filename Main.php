0ffb7bef3f19e3db9f4c313dd464bbee302c26b5
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-09-24 20:01:17 +1000

    Update Main.php

--- Main.php
+++ Main.php
@@ -1,1 +1,27 @@
-?
+<?php
+
+namespace imtoolazyfixthiscode;
+
+
+class Main {
+
+public function onLoad(): void {
+$echo ("Fixing code");
+
+  $codetofix = '/';
+
+
+
+  function d() {
+$world = "$level";
+
+
+    
+
+
+  }
+}
+
+}
+
+#coming soon im tired
